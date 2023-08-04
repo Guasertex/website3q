50ccc9a83e18a80bac25335422446f554246d678
Guasertex <markus9999123@gmail.com>
2023-08-04 15:25:29 +0300

    fd

--- server/api/hello.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler(async event => {
-  const query =  getQuery(event);
-  const body = await readBody(event);
-  return {
-    api: "works bitch",
-    record: query.param1,
-    body: body
-  };
-});
